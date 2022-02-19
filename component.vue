9bcd87d1357bf19cf0ff42d73993f3df84257e1d
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-02-19 12:47:18 -0600

    Prueba a rama footer

---
+++ component.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ mensaje }}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mensaje: "Hola a todos",
+        };
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: bisque;
+    font-style: italic;
+}
+</style>
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const arrow = () => {
-    let sum = 2 + 2
-    return sum  
-}
+    let sum = 2 + 2;
+    const inner = () => {
+        let hello = "Hello World";
+        let message = `${hello} my friends`;
+        if (false) {
+            console.log("Hello there")
+        }
+        console.log(message);
+    };
+    return inner;
+};
 
-arrow()
+arrow();
